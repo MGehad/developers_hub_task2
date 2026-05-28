27db2f76ff98fe5e02d3c43e7a7493b9b018b907
Mohamed Gehad <134580414+MGehad@users.noreply.github.com>
2026-05-28 18:32:11 +0300

    feat: Integrate Firebase authentication and core services

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:developers_hub_task2/app.dart';
 import 'package:developers_hub_task2/core/services/local_database/local_database.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await LocalDatabase.init();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(const MyApp());
 }
